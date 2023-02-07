c48fbc0f4c2fcf1d3cba119e37932dd5aa358191
Luke Mattfeld <lukemattfeld@gmail.com>
2023-02-07 13:09:21 -0800

    fixed rust build

--- rust-aws-lambda/src/main.rs
+++ rust-aws-lambda/src/main.rs
@@ -1,15 +1,15 @@
 // This example requires the following input to succeed:
 // { "command": "do something" }
 
+use lambda_http::{lambda_runtime::Error, run, service_fn, Body, Request, Response};
+use lazy_static::lazy_static;
 use log::LevelFilter;
+use rusoto_core::Region;
+use rusoto_dynamodb::{AttributeValue, DynamoDb, DynamoDbClient, PutItemInput};
 use serde::{Deserialize, Serialize};
 use simple_logger::SimpleLogger;
-use rusoto_core::{Region};
-use rusoto_dynamodb::{DynamoDb, DynamoDbClient, PutItemInput, AttributeValue, PutItemError, PutItemOutput};
 use std::collections::HashMap;
 use uuid::Uuid;
-use lazy_static::lazy_static;
-use lambda_http::{handler, lambda_runtime::{self, Context, Error}, IntoResponse, Request, Body, Response};
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Book {
@@ -20,30 +20,29 @@ struct Book {
 
 //use lazy to speed up cold start(use CPU burst on start up)
 lazy_static! {
-   static ref CLIENT:DynamoDbClient=DynamoDbClient::new(Region::UsEast2);
+    static ref CLIENT: DynamoDbClient = DynamoDbClient::new(Region::UsEast2);
 }
-
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), Error> {
-    SimpleLogger::new().with_level(LevelFilter::Info).init().unwrap();
-    lambda_runtime::run(handler(func)).await?;
+    SimpleLogger::new()
+        .with_level(LevelFilter::Info)
+        .init()
+        .unwrap();
+    let func = service_fn(func);
+    run(func).await?;
     Ok(())
 }
 
-async fn func(event: Request, _: Context) -> Result<impl IntoResponse, Error> {
+async fn func(event: Request) -> Result<Response<Body>, Error> {
     Ok(match event.body() {
         Body::Text(body) => {
-            let book_as_string = serde_json::to_string(&handle_body(body).await)?;
-             Response::builder()
+            let book_as_string = serde_json::to_string(&handle_body(&body).await)?;
+            Response::builder()
                 .status(201)
                 .body::<Body>(book_as_string.into())?
-        },
-        _ =>
-             Response::builder()
-                .status(400)
-                .body("Empty body".into())?
-        ,
+        }
+        _ => Response::builder().status(400).body("Empty body".into())?,
     })
 }
 
@@ -52,9 +51,27 @@ async fn handle_body(body: &String) -> Book {
     let mut map = HashMap::new();
     let uuid = Uuid::new_v4().to_string();
     book.id = Some(uuid.clone());
-    map.insert("id".to_string(), AttributeValue { s: Some(book.id.clone().unwrap()), ..AttributeValue::default() });
-    map.insert("name".to_string(), AttributeValue { s: Some(book.name.clone()), ..AttributeValue::default() });
-    map.insert("author".to_string(), AttributeValue { s: Some(book.author.clone()), ..AttributeValue::default() });
+    map.insert(
+        "id".to_string(),
+        AttributeValue {
+            s: Some(book.id.clone().unwrap()),
+            ..AttributeValue::default()
+        },
+    );
+    map.insert(
+        "name".to_string(),
+        AttributeValue {
+            s: Some(book.name.clone()),
+            ..AttributeValue::default()
+        },
+    );
+    map.insert(
+        "author".to_string(),
+        AttributeValue {
+            s: Some(book.author.clone()),
+            ..AttributeValue::default()
+        },
+    );
 
     let put_item = PutItemInput {
         item: map,
